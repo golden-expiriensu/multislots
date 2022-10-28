b23b6892c33c20f84edb5e1c447374c82a361008
golden-expiriensu <gundari_dan@bk.ru>
2022-10-28 14:01:49 +0700

    resolved dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,6 @@
 import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "hardhat-contract-sizer";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
@@ -17,40 +15,10 @@ const config: HardhatUserConfig = {
   namedAccounts: {
     deployer: 0,
   },
-  networks: {
-    rinkeby: {
-      url: process.env.RINKEBY_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    bscTestnet: {
-      url: process.env.BCS_TESTNET_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    polygonMumbai: {
-      url: process.env.MUMBAI_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    avalancheFujiTestnet: {
-      url: process.env.AVAX_FUJI || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
-  },
-  etherscan: {
-    apiKey: {
-      rinkeby: process.env.ETHERSCAN_API_KEY ?? '',
-      bscTestnet: process.env.BSCSCAN_API_KEY ?? '',
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY ?? '',
-      avalancheFujiTestnet: process.env.AVASCAN_API_KEY ?? '',
-    },
-  },
+  }
 };
 
 export default config;
